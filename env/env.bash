29a06f3ebd8269a927514911e090623e9e0d4c62
jerq <jerq@jerq.fi>
2026-09-09 09:02:16 +0300

    Added OpenCode to PATH, Removed Debian/Ubuntu from Ansible

--- env/env.bash
+++ env/env.bash
@@ -1,4 +1,5 @@
 export GOPATH="$HOME/go"
-export PATH="$PATH:$GOPATH/bin:$HOME/.cargo/bin:$HOME/.local/bin"
+export PATH="$PATH:$GOPATH/bin:$HOME/.cargo/bin:$HOME/.local/bin":$HOME/.opencode/bin:$PATH
 
 command -v zoxide &>/dev/null && eval "$(zoxide init bash)"
+
