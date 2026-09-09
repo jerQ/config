84cf8fbe65565bd85cfd07ebca7d13e436c1f719
jerq <jerq@jerq.fi>
2026-09-09 09:49:13 +0300

    Added OLLAMA_HOST to bash

--- env/env.bash
+++ env/env.bash
@@ -1,5 +1,6 @@
 export GOPATH="$HOME/go"
 export PATH="$PATH:$GOPATH/bin:$HOME/.cargo/bin:$HOME/.local/bin":$HOME/.opencode/bin:$PATH
+export OLLAMA_HOST=127.0.0.1:11434
 
 command -v zoxide &>/dev/null && eval "$(zoxide init bash)"
 
