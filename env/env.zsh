9c7a63bcece617a2d43c6b137cebbe0e375bfac4
jerq <jerq@jerq.fi>
2026-09-24 15:09:56 +0300

    Moved Zoxide to the end of the file

--- env/env.zsh
+++ env/env.zsh
@@ -1,5 +1,5 @@
 export GOPATH="$HOME/go"
 export PATH="$PATH:$GOPATH/bin:$HOME/.cargo/bin:$HOME/.local/bin:$HOME/.opencode/bin"
 
-command -v zoxide &>/dev/null && eval "$(zoxide init zsh)"
 command -v starship &>/dev/null && eval "$(starship init zsh)"
+command -v zoxide &>/dev/null && eval "$(zoxide init zsh)"
